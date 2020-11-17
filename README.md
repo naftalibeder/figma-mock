de57d9572d197bece8704f6dbcc849bc8997c679
Naftali Beder <naftalibeder@gmail.com>
2020-11-16 19:37:51 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Description
+
+A Figma plugin for quickly inserting predefined text into specific text layers.
+
+# Setup
+
+Install TypeScript globally:
+
+  npm install -g typescript
+
+Install local dependencies:
+
+  npm install
+
+Use Visual Studio Code to compile TypeScript to JavaScript by running the "Terminal > Run Build Task..." menu item and selecting "tsc: watch - tsconfig.json".
---
+++ src/main.ts
@@ -0,0 +1,59 @@
+figma.showUI(__html__, { width: 240, height: 400 });
+
+const onStart = () => {
+  const selectedNodes: readonly SceneNode[] = figma.currentPage.selection;
+
+  const nodes = getAllTextNodes(selectedNodes);
+  const uniqueContents = getUniqueTextNodeContents(nodes);
+
+  figma.ui.postMessage({ type: 'init', uniqueContents });
+};
+
+const onPressConfirm = (listText: string, selectionText: string) => {
+  const selectedNodes: readonly SceneNode[] = figma.currentPage.selection;
+
+  const listLines: string[] = listText.split('\n').filter(line => line.length > 0);
+  const textNodes: TextNode[] = getTextNodesContainingText(selectedNodes, selectionText);
+
+  textNodes.forEach(async (textNode, index) => {
+    await figma.loadFontAsync(textNode.fontName as FontName);
+    textNode.characters = listLines[index % listLines.length];
+  });
+};
+
+const appendChildTextNodes = (nodes: TextNode[], node: BaseNode) => {
+  if (node.type === 'FRAME' || node.type === 'GROUP' || node.type === 'INSTANCE') {
+    const childNodes: TextNode[] = node.children.filter(child => child.type === 'TEXT') as TextNode[];
+    Array.prototype.push.apply(nodes, childNodes);
+    node.children.forEach((node) => appendChildTextNodes(nodes, node));
+  } else if (node.type === 'TEXT') {
+    nodes.push(node);
+  }
+}
+
+const getAllTextNodes = (selectedNodes: readonly SceneNode[]) => {
+  let all: TextNode[] = [];
+  selectedNodes.forEach((node) => appendChildTextNodes(all, node));
+  return all;
+};
+
+const getUniqueTextNodeContents = (nodes: TextNode[]) => {
+  const contents = nodes.map((node: TextNode) => node.characters);
+  return Array.from(new Set(contents));
+}
+
+const getTextNodesContainingText = (selectedNodes: readonly SceneNode[], text: string) => {
+  const all: TextNode[] = getAllTextNodes(selectedNodes);
+  return all.filter(node => node.characters == text);
+}
+
+figma.ui.onmessage = msg => {
+  if (msg.type === 'init') {
+    onStart();
+  } else if (msg.type === 'confirm') {
+    onPressConfirm(msg.listText, msg.selectionText);
+    figma.closePlugin();
+  } else if (msg.type === 'cancel') {
+    figma.closePlugin();
+  }
+};
