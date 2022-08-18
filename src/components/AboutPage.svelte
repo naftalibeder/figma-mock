2c17a137a8b31d4177380970bc28f866c6cf2302
Naftali Beder <naftali@naftalibeder.com>
2022-08-18 00:16:53 -0500

    Adjust layout

--- src/components/AboutPage.svelte
+++ src/components/AboutPage.svelte
@@ -2,24 +2,31 @@
   import { Type } from "figma-plugin-ds-svelte";
 </script>
 
-<div class="flex flex-1 flex-col p-4 gap-2">
-  <Type
-    >Mock is developed by Naftali Beder and is completely free to use. The code is open source and
-    available <a href="https://github.com/naftalibeder/figma-mock" target="_blank"> here</a>.
-  </Type>
-  <Type
-    >Bug reports and feature requests are welcome, and can be posted as issues on the GitHub page,
-    or sent to <a href="mailto:naftali@naftalibeder.com" target="_blank">naftali@naftalibeder.com</a
-    >.</Type
-  >
-  <Type
-    >If you find Mock useful, consider clicking the heart icon on its <a
-      href="https://www.figma.com/community/plugin/908371597321736295"
-      target="_blank"
-    >
-      product page</a
-    >, leaving a comment, or telling someone else about it.</Type
-  >
-  <Type>Thank you for using it!</Type>
-  <Type>- Naftali</Type>
+<div>
+  <div class="section pt-4">
+    <div class="flex flex-col px-2 gap-2">
+      <Type
+        >Mock is developed by Naftali Beder and is completely free to use. The code is open source
+        and available <a href="https://github.com/naftalibeder/figma-mock" target="_blank">
+          here</a
+        >.
+      </Type>
+      <Type
+        >Bug reports and feature requests are welcome, and can be posted as issues on the GitHub
+        page, or sent to <a href="mailto:naftali@naftalibeder.com" target="_blank"
+          >naftali@naftalibeder.com</a
+        >.</Type
+      >
+      <Type
+        >If you find Mock useful, consider clicking the heart icon on its <a
+          href="https://www.figma.com/community/plugin/908371597321736295"
+          target="_blank"
+        >
+          product page</a
+        >, leaving a comment, or telling someone else about it.</Type
+      >
+      <Type>Thank you for using it!</Type>
+      <Type>- Naftali</Type>
+    </div>
+  </div>
 </div>
